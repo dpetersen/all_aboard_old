64127490c84d5029682beff36cff725a3e666a57
Don Petersen <don@donpetersen.net>
2012-03-30 22:25:46 -0700

    Initial commit with Rails skeleton.

---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+AllAboard::Application.config.session_store :cookie_store, key: '_all_aboard_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# AllAboard::Application.config.session_store :active_record_store
