666278a7255e20e7202a4d0145978f2afa2cac86
Don Petersen <don@donpetersen.net>
2012-03-31 19:36:21 -0700

    Added a test I missed in SourceManager refactoring.

--- spec/lib/source_manager_spec.rb
+++ spec/lib/source_manager_spec.rb
@@ -6,6 +6,17 @@ end
 describe SourceManager do
   let(:source_manager) { SourceManager.new }
 
+  describe ".instance" do
+    it "returns an instance of itself" do
+      SourceManager.instance.should be_a(SourceManager)
+    end
+
+    it "returns the same instance for every call" do
+      first_instance = SourceManager.instance
+      SourceManager.instance.should == first_instance
+    end
+  end
+
   describe "#source_for_name" do
     subject { source_manager.source_for_name("SourceManagerSpecTest") }
 
