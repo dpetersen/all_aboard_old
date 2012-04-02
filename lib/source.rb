701009a6c767bf48a5e3f6b468314e463d8817c2
Don Petersen <don@donpetersen.net>
2012-04-01 21:44:10 -0700

    Refactor a bunch of Source's concerns into modules.
    Maybe a sign that it does too much, but I'm not too worried about it
    quite yet.

--- lib/source.rb
+++ lib/source.rb
@@ -1,59 +1,16 @@
 class Source
   extend Source::Persistence
-
-  cattr_accessor :configurable_attributes
-
-  def self.has_job(job_class, frequency)
-    @jobs_for_frequencies ||= {}
-    @jobs_for_frequencies[frequency] ||= []
-    @jobs_for_frequencies[frequency] << job_class
-  end
-
-  def self.jobs_for_frequencies
-    @jobs_for_frequencies || {}
-  end
+  extend Source::Jobs
+  extend Source::Perspectives
+  include Source::Configuration
 
   def self.filesystem_name
     self.name.underscore
   end
 
-  def self.perspective(perspective_name)
-    @perspectives ||= []
-    @perspectives << Perspective.new(self, perspective_name)
-  end
-
-  def self.perspectives
-    @perspectives
-  end
-
-  def self.perspective_names
-    perspectives.map(&:name)
-  end
-
-  def configuration
-    self.class.configurable_attributes.inject({}) do |h, attribute|
-      h[attribute] = configured_attribute_for(attribute).try(:value)
-      h
-    end
-  end
-
   def render(perspective_name, pane)
-    perspective = perspective_for_name(perspective_name)
+    perspective = self.class.perspective_for_name(perspective_name)
     context = PerspectiveRenderingContext.new(self)
     perspective.template_for_pane(pane).render(context).strip
-  end
-
-protected
-
-  def configured_attributes
-    ConfiguredAttribute.where(source_name: self.class.name)
-  end
-
-  def configured_attribute_for(attribute)
-    configured_attributes.where(name: attribute).first
-  end
-
-  def perspective_for_name(perspective_name)
-    self.class.perspectives.detect { |p| p.name == perspective_name }
   end
 end
---
+++ lib/source/configuration.rb
@@ -0,0 +1,24 @@
+module Source::Configuration
+  extend ActiveSupport::Concern
+
+  module ClassMethods
+    attr_accessor :configurable_attributes
+  end
+
+  def configuration
+    self.class.configurable_attributes.inject({}) do |h, attribute|
+      h[attribute] = configured_attribute_for(attribute).try(:value)
+      h
+    end
+  end
+
+protected
+
+  def configured_attributes
+    ConfiguredAttribute.where(source_name: self.class.name)
+  end
+
+  def configured_attribute_for(attribute)
+    configured_attributes.where(name: attribute).first
+  end
+end
